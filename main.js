1f2ae9968fa8848eb762bcfd98d64db6f035dbd2
밖쓰 <101634719+parkingbox@users.noreply.github.com>
2022-04-04 03:18:27 +0900

    20220403 Portfolio Website_main.js

--- main.js
+++ main.js
@@ -12,6 +12,7 @@ document.addEventListener('scroll', () => {
     }
 });
 
+
 // Handle scrolling when tapping on the navbar menu
 const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click', (event) => {
@@ -20,9 +21,15 @@ navbarMenu.addEventListener('click', (event) => {
     if (link == null) {
         return;
     }
+    navbarMenu.classList.remove('open');
     scrollIntoView(link);
 });
 
+// Navbar toggle button for small screen
+const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
+navbarToggleBtn.addEventListener('click', (event) => {
+    navbarMenu.classList.toggle('open');
+});
 
 // Handle click on "contact me" button on home
 const homeContactBtn = document.querySelector('.home__contact');
